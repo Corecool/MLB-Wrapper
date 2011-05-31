8ab6b55e7db83c52a1fa790a058881d2b7a11e1b
Corecool <wowwowxyr@126.com>
2011-05-31 21:00:55 +0800

    实现Cache组件
    准备利用gen_server实现LIRS组件，现在编好首个单元测试，下次PASS它。
    对eunit/makefile进行修改，利用erlc本身能力。

--- eunit/lirs_tests.erl
+++ eunit/lirs_tests.erl
@@ -1,14 +1,46 @@
 -module(lirs_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-simple_test() ->
-    ?assert(1 + 1 =:= 2).
+-record(cache, {resList = [],hirList = [],lirList = []}).
+-record(resource,{id,status = none,location = []}).
 
-falied_test() ->
-    ?assertEqual(3,1+1).
+create_res() ->
+    Seq = lists:seq(0,9),
+    lists:foldl(fun(X,List) ->
+			[#resource{id = X} | List]
+		end,
+		[],Seq).
 
-testset_test_() ->
-    ?_assertEqual(5,3+2),
-    ?_assertError(badarith, 10/0),
-    ?_assertEqual(100,5).
+init_test_() ->
+    ResList = create_res(),
+    State = #cache{resList = ResList,
+			     hirList = [],
+			     lirList = []},
+    ?_assertMatch({ok,State},lirs:init(10)).
 
+first_element_test_() ->
+    {spawn,
+     {setup,
+      fun() ->
+	      lirs:start_link(10)
+      end,
+      fun(_) ->
+	      gen_server:cast(lirs,stop)
+      end,
+      ?_test(
+	 begin
+	     {Res,NewState} = gen_server:call(lirs,
+					      #resource{id = 6}),
+	     ExpectState = #cache{resList = create_res(),
+				  hirList = [],
+				  lirList = [Res]},
+	     ?assertEqual(#resource{id = 6,status = lir,
+				    location = [self()]},Res),
+	     ?assertEqual(ExpectState,NewState)
+	 end
+	)}}.
+
+    
+    
+    
+   
--- src/lirs.erl
+++ src/lirs.erl
@@ -1,5 +1,138 @@
+%%%-------------------------------------------------------------------
+%%% @author Corecool <>
+%%% @copyright (C) 2011, Corecool
+%%% @doc
+%%%
+%%% @end
+%%% Created : 31 May 2011 by Corecool <>
+%%%-------------------------------------------------------------------
 -module(lirs).
--compile(export_all).
 
-test() ->
-    "Hello,world".
+-behaviour(gen_server).
+
+%% API
+-export([start_link/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-record(cache, {resList = [],hirList = [],lirList = []}).
+-record(resource,{id,status = none,location = []}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link(ResNum) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, ResNum, []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init(ResNum) ->
+    Seq = lists:seq(0,ResNum - 1),
+    ResList = lists:foldl(fun(X,List) ->
+				  [#resource{id = X} | List]
+			  end,
+			  [],Seq),
+    {ok, #cache{resList = ResList}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(Request, _From, State) ->
+    Reply = {Request,State},
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(Msg, State) ->
+    case Msg of
+	stop ->
+	    {stop,normal,State};
+	_ ->
+	    {noreply, State}
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
